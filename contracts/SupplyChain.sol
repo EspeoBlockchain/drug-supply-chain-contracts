07cdec989cf7f90adbeae6e029230186f7ad8bea
Tomasz Cichowicz <tomasz.cichowicz@espeo.eu>
2019-04-15 11:08:27 +0200

    Disallow registering same package twice

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -14,8 +14,16 @@ contract SupplyChain {
 
     mapping(bytes => PackageInfo) private packageLog;
 
-    function registerInitialTransfer(bytes memory _packageId, address _to, ReceiverType _receiver) public {
+    function registerInitialTransfer(bytes memory _packageId, address _to, ReceiverType _receiver)
+        public
+        onlyNewPackages(_packageId)
+    {
         packageLog[_packageId] = PackageInfo(_packageId, msg.sender, _to, _receiver);
+    }
+
+    modifier onlyNewPackages(bytes memory _packageId) {
+        require(packageLog[_packageId].packageId.length == 0, "Given package is already known");
+        _;
     }
 
     function getPackageInfo(bytes memory _packageId) public view returns (PackageInfo memory) {
--- test/SupplyChain.test.js
+++ test/SupplyChain.test.js
@@ -1,4 +1,10 @@
-/* globals artifacts, contract, expect, it, web3 */
+/* globals artifacts, contract, it, web3 */
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
 
 const SupplyChain = artifacts.require('SupplyChain');
 const { hexToBytes, randomHex } = web3.utils;
@@ -32,5 +38,15 @@ contract('SupplyChain', async (accounts) => {
       const actual = await sut.getPackageInfo.call(packageIdBytes);
       expect(actual.receiverType).to.equal(`${receiverType}`);
     });
+  });
+
+  it('should not allow registering same package twice', async () => {
+    // given
+    const sut = await SupplyChain.new();
+    await sut.registerInitialTransfer(packageIdBytes, to, receiver, { from });
+    // when
+    const promise = sut.registerInitialTransfer(packageIdBytes, to, receiver, { from });
+    // then
+    await expect(promise).to.be.rejectedWith('Given package is already known');
   });
 });
