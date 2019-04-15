3d3e837ed7b8ea33b10f21a1cb610e39ec2a4642
Tomasz Cichowicz <tomasz.cichowicz@espeo.eu>
2019-04-15 10:45:15 +0200

    Added ReceiverType enum

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -7,12 +7,15 @@ contract SupplyChain {
         bytes packageId;
         address from;
         address to;
+        ReceiverType receiverType;
     }
+
+    enum ReceiverType { Transporter, Pharmacy }
 
     mapping(bytes => PackageInfo) private packageLog;
 
-    function registerInitialTransfer(bytes memory _packageId, address _to) public {
-        packageLog[_packageId] = PackageInfo(_packageId, msg.sender, _to);
+    function registerInitialTransfer(bytes memory _packageId, address _to, ReceiverType _receiver) public {
+        packageLog[_packageId] = PackageInfo(_packageId, msg.sender, _to, _receiver);
     }
 
     function getPackageInfo(bytes memory _packageId) public view returns (PackageInfo memory) {
--- test/SupplyChain.test.js
+++ test/SupplyChain.test.js
@@ -4,19 +4,33 @@ const SupplyChain = artifacts.require('SupplyChain');
 const { hexToBytes, randomHex } = web3.utils;
 
 contract('SupplyChain', async (accounts) => {
+  const packageId = randomHex(32);
+  const packageIdBytes = hexToBytes(packageId);
+  const receiver = 0; // Transporter
+  const from = accounts[0];
+  const to = accounts[1];
+
   it('should register initial transfer', async () => {
     // given
-    const sut = await SupplyChain.deployed();
-    const packageId = randomHex(32);
-    const packageIdBytes = hexToBytes(packageId);
-    const from = accounts[0];
-    const to = accounts[1];
+    const sut = await SupplyChain.new();
     // when
-    await sut.registerInitialTransfer(packageIdBytes, to, { from });
+    await sut.registerInitialTransfer(packageIdBytes, to, receiver, { from });
     // then
     const actual = await sut.getPackageInfo.call(packageIdBytes);
     expect(actual.packageId).to.equal(packageId);
     expect(actual.to).to.equal(to);
     expect(actual.from).to.equal(from);
+  });
+
+  ['Transporter', 'Pharmacy'].forEach((receiverName, receiverType) => {
+    it(`should allow ${receiverName} receiver type`, async () => {
+      // given
+      const sut = await SupplyChain.new();
+      // when
+      await sut.registerInitialTransfer(packageIdBytes, to, receiverType, { from });
+      // then
+      const actual = await sut.getPackageInfo.call(packageIdBytes);
+      expect(actual.receiverType).to.equal(`${receiverType}`);
+    });
   });
 });
