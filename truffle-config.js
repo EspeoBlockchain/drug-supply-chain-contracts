06a3fecd92597d9361fb98c696240ca1a627c7ae
Tomasz Cichowicz <tomasz.cichowicz@espeo.eu>
2019-04-25 09:36:01 +0200

    Config for migrating contracts to Ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
+  networks: {
+    ropsten: {
+      provider: () => new HDWalletProvider(
+        process.env.PRIVATE_KEY || process.env.MNEMONIC,
+        process.env.ROPSTEN_PROVIDER_URL,
+      ),
+      network_id: '3',
+    },
+  },
   compilers: {
     solc: {
       version: '0.5.7',
