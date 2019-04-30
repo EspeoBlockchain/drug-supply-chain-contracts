4c56b24e91e2452a3eaa44535b65204e48087c9e
Tomasz Cichowicz <tomasz.cichowicz@espeo.eu>
2019-04-30 14:19:40 +0200

    Refactor purchasability codes expect in a more functional manner

--- test/common/expectPurchasabilityCodes.js
+++ test/common/expectPurchasabilityCodes.js
@@ -1,21 +1,18 @@
 const expect = require('./expect');
-const expectBignumber = require('./expectBignumber');
 
-const { hexToNumber } = web3.utils;
+const { hexToNumberString } = web3.utils;
 
 const expectedMaximumLength = 10;
 
 module.exports = actualCodes => ({
   toEqual: (expectedCodes) => {
-    expect(actualCodes.length).to.equal(expectedMaximumLength);
+    const expected = [
+      ...expectedCodes.map(hexToNumberString),
+      ...Array(expectedMaximumLength - expectedCodes.length).fill('0'),
+    ];
 
-    for (let i = 0; i < expectedCodes.length; i += 1) {
-      const actualCode = hexToNumber(actualCodes[i]);
-      expect(actualCode).to.equal(expectedCodes[i]);
-    }
+    const actual = actualCodes.map(String);
 
-    for (let i = expectedCodes.length; i < expectedMaximumLength; i += 1) {
-      expectBignumber(actualCodes[i]).toEqual(0);
-    }
+    expect(actual).to.deep.equal(expected);
   },
 });
