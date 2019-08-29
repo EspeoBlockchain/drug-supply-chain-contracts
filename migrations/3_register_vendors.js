1b93d26e3ea29d7e45f780f46217b7f8d3537eff
krzysztofwedrowicz <krzysztof.wedrowicz@espeo.eu>
2019-08-29 11:28:17 +0200

    Add missing vendor

--- migrations/3_register_vendors.js
+++ migrations/3_register_vendors.js
@@ -6,6 +6,7 @@ const vendors = [
   '0xead172adb6cd5fe0b58150301ca25598c0dd1613',
   '0x18b0de3577c6f97522b1335344ad3d05cf3a6e2c',
   '0x75711d62a3db9f945ed901fea25f2ba6ae4cc5cc',
+  '0x1d4a5d98356eb2db352294c07156a8f4ef70f161',
 ];
 
 module.exports = async (deployer) => {
